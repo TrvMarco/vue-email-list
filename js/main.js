a757c084eea5467b92a8c8360a91df14ca6cc1c9
TrvMarco <trevisanello.marco@gmail.com>
2022-05-30 16:52:34 +0200

    aggiunto stampa delle email

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
 const app = new Vue({
     el: '#root',
     data: {
-       mails: [
-           
-       ]
+       mails: []
     },
     created(){
         
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response)=> {
-            console.log(response.data.response);
-            while(this.mails.length < 10){
+        for(let i = 0; i < 10; i++){
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response)=> {
                 const newMail = {
                     mail: response.data.response
                 }
                 this.mails.push(newMail)
-            }
-            console.log(response.data.response);
-        })
+            })
+        }
     }
 })
