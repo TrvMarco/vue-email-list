054b7c8fd6204faa7c0851d5e857e442eb342677
TrvMarco <trevisanello.marco@gmail.com>
2022-05-30 16:38:48 +0200

    aggiunto azione e ciclo per aggiungere oggetti ad array

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 const app = new Vue({
     el: '#root',
     data: {
-       
+       mails: [
+           
+       ]
+    },
+    created(){
+        
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response)=> {
+            console.log(response.data.response);
+            while(this.mails.length < 10){
+                const newMail = {
+                    mail: response.data.response
+                }
+                this.mails.push(newMail)
+            }
+            console.log(response.data.response);
+        })
     }
 })
